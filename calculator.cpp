ab3ee99e701a53eb4e043844c1dcb0638b877234
sinamna <sina3050@gmail.com>
2021-02-04 23:01:51 +0330

    added convertToPostFix thingy

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 #include "DLinkedList.h"
 #include "Node.h"
 using namespace std;
@@ -7,9 +8,21 @@ using namespace std;
 class Calculator{
     private:
         LinkedList* list;
+
         //unordered_map for hashing
         string expr;
-
+    int precLevel(char ch){
+        switch (ch){
+            case '+':
+                return 2;
+            case '-':
+                return 2;
+            case '*':
+                return 3;
+            case '(':
+                return 1;
+        }
+    }
     public:
 
     Calculator(string expr){
@@ -21,7 +34,42 @@ class Calculator{
     //calculating 
 
     //converting infix to postfix
-    
+    string convertToPostfix(){
+        string operands="()*+-";
+        string result="";
+        stack<char> opstack;
+        for (int i=0;i<expr.length();i++){
+            char ch = expr.at(i);
+            size_t operand_found=operands.find(ch);
+            if(operand_found != string::npos){
+                if(ch == '(')
+                    opstack.push(ch);
+                else if(ch == ')'){
+                    char top = opstack.top();
+                    opstack.pop();
+                    while(top!= '('){
+                        result.push_back(top);
+                        top =opstack.top();
+                        opstack.pop();
+                    }
+                }else{
+                    while(!opstack.empty() && (precLevel(opstack.top()) >= precLevel(ch))){
+                        result.push_back(opstack.top());
+                        opstack.pop();
+                    }
+                    opstack.push(ch);
+                }
+
+            }else{
+                result.push_back(ch);
+            }
+        }
+        while(!opstack.empty()){
+            result.push_back(opstack.top());
+            opstack.pop();
+        }
+        return result;
+    }
     //move cursor to right
     void moveCurserToRight(){
         list->moveCursorToRight();
